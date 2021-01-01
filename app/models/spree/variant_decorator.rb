b15573dedb490b5829b1fc2daf43734228b90753
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2021-01-02 00:36:35 +0800

    fix product variant clone issue. hide cost field in variant pricing

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -12,5 +12,13 @@ module Spree::VariantDecorator
     values.to_sentence({ words_connector: ", ", two_words_connector: ", " })
   end
 
+  private
+
+  def enforce_unique_sku?
+    # I had to force remove this check due to issue on cloning previously cloned variant but got deleted
+    # !deleted_at
+    false
+  end
+
   Spree::Variant.prepend self
 end
